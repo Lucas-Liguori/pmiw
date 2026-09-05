5e707d93e04e57cf775f67a5ff352590cd0939c7
Lucas-Liguori <128557594+Lucas-Liguori@users.noreply.github.com>
2026-09-05 01:02:27 -0300

    tp1

---
+++ tp1.js
@@ -0,0 +1,31 @@
+
+
+
+let Bosque = [];
+let Rata = [];
+let Fogata = [];
+let Jaguar = [];
+
+function preload() {
+
+
+  cargarImagenes(Bosque);
+  cargarImagenes(1, 6, Rata);
+  cargarImagenes(1, 6, Fogata);
+  cargarImagenes(1, 8, Jaguar);
+
+
+function setup() {
+ createCanvas(800, 600 );
+ 
+ 
+ 
+}
+
+
+function draw() {
+background( 200 );
+
+
+
+}
